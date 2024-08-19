d0b868f2a1af45301ad257107a8dce10218b3fb8
stg11077 <stg11077@gmail.com>
2024-08-20 00:17:40 +0300

    add unvers migration

---
+++ MessengerBackend.Storage/Migrations/20240814200335_addTableUserStatistic.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessengerBackend.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTableUserStatistic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserRequestStatistics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RequestCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRequestStatistics", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRequestStatistics");
+        }
+    }
+}
